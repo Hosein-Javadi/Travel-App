59f5332922228488e416c880cbff2282c99a493c
hosein12345678 <h.javadi8212@gmail.com>
2024-09-09 02:44:56 +0330

    writed logic of splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:aspen_explore_application/bindings/bindings.dart';
 import 'package:aspen_explore_application/screens/root.dart';
+import 'package:aspen_explore_application/screens/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,28 +12,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final bool? viwedSplash = GetStorage().read('splashState');
     return GetMaterialApp(
       initialBinding: AppBindings(),
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: const ColorScheme.light(
           brightness: Brightness.light,
           primary: Color(0xff176FF2),
@@ -41,7 +28,17 @@ class MyApp extends StatelessWidget {
           onSurface: Color(0xff0000),
         ),
       ),
-      home: const RootScreen(),
+      getPages: [
+        GetPage(
+          name: '/root',
+          page: () => const RootScreen(),
+        ),
+        GetPage(
+          name: '/splash',
+          page: () => const SplashScreen(),
+        )
+      ],
+      initialRoute: viwedSplash != null ? '/root' : '/splash',
     );
   }
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,8 +1,17 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
